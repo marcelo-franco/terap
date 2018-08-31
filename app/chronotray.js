9c7551c78dc215025ab646a8555d0f3194679147
Marcelo Franco <marcelo.franco.br@gmail.com>
2018-08-31 12:16:33 -0300

    reorganizando o código parte 2

--- app/chronotray.js
+++ app/chronotray.js
@@ -2,8 +2,29 @@ const electron = require('electron');
 const { Tray } = electron;
 
 class ChronoTray extends Tray {
-    constructor(iconPath) {
+    constructor(iconPath, mainWindow) {
         super(iconPath); // aqui, estou chamando o construtor do Tray
+        this.mainWindow = mainWindow;
+        this.on('click', this.onClick.bind(this));
+    }
+
+    onClick(event, bounds) {
+        //coordenadas do ícone da bandeja
+        const { x, y } = bounds
+        //dimensões da janela (largura e altura)
+        const { width, height } = this.mainWindow.getBounds();
+
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide();
+        } else {
+            this.mainWindow.setBounds({
+                x: x >= 400 ? x - width / 2 : x,
+                y: y >= 300 ? y - height : y,
+                width,
+                height 
+            });
+            this.mainWindow.show();
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 const electron = require('electron');
 const ChronoTray = require('./app/chronotray');
-const { app, Tray, Menu, BrowserWindow } = electron;
+const { app, Menu, BrowserWindow } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-    const tray = new ChronoTray(`${__dirname}/robot.png`);
-    tray.setToolTip('Esta é uma aplicação Electron');
-    tray.on('click', (event, bounds) => {
-        //coordenadas do ícone da bandeja
-        const { x, y } = bounds
-        //dimensões da janela (largura e altura)
-        const { width, height } = mainWindow.getBounds();
-
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            mainWindow.setBounds({
-                x: x >= 400 ? x - width / 2 : x,
-                y: y >= 300 ? y - height : y,
-                width,
-                height 
-            });
-            mainWindow.show();
-        }
-    });
-    const contextMenu = Menu.buildFromTemplate(menuTemplate);
-    tray.setContextMenu(contextMenu);
-
     mainWindow = new BrowserWindow({
         height: 150,
         width: 300,
@@ -35,6 +12,11 @@ app.on('ready', () => {
         resizable: false,
         show: false
     });
+    const tray = new ChronoTray(`${__dirname}/robot.png`, mainWindow);
+    tray.setToolTip('Esta é uma aplicação Electron');
+    const contextMenu = Menu.buildFromTemplate(menuTemplate);
+    tray.setContextMenu(contextMenu);
+
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
