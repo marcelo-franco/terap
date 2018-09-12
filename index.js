2fb29350dc442c0cbfe4b0df745e4bd5f98acc69
Marcelo Franco <marcelo.franco.br@gmail.com>
2018-09-12 17:26:45 -0300

    Atualizações automáticas - parte 1

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const electron = require('electron');
 const ChronoTray = require('./app/chronotray');
 const ws = require('windows-shortcuts');
-const { app, Menu, BrowserWindow, ipcMain, autoUpdater, dialog } = electron;
-
-const isDev = require('electron-is-dev');
-const server = 'https://hazel-server-jticjoauhu.now.sh'
-const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+const { app, Menu, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 let tray;
@@ -18,52 +14,52 @@ if (handleSquirrelEvent()) {
    // squirrel event handled and app will exit in 1000ms, so don't do anything else
    return;
 }
-function handleSquirrelEvent() {
-    if (process.argv.length === 1) {
-        return false;
-    }
+ function handleSquirrelEvent() {
+   if (process.argv.length === 1) {
+     return false;
+   }
     const ChildProcess = require('child_process');
-    const path = require('path');
+   const path = require('path');
     const appFolder = path.resolve(process.execPath, '..');
-    const rootAtomFolder = path.resolve(appFolder, '..');
-    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-    const exeName = path.basename(process.execPath);
+   const rootAtomFolder = path.resolve(appFolder, '..');
+   const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+   const exeName = path.basename(process.execPath);
     const spawn = function(command, args) {
-    let spawnedProcess, error;
-    try {
-        spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-    } catch (error) {}
-        return spawnedProcess;
-    };
+     let spawnedProcess, error;
+      try {
+       spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+     } catch (error) {}
+      return spawnedProcess;
+   };
     const spawnUpdate = function(args) {
-        return spawn(updateDotExe, args);
-    };
+     return spawn(updateDotExe, args);
+   };
     const squirrelEvent = process.argv[1];
-    switch (squirrelEvent) {
+   switch (squirrelEvent) {
      case '--squirrel-install':
      case '--squirrel-updated':
-        // Optionally do things such as:
-        // - Add your .exe to the PATH
-        // - Write to the registry for things like file associations and
-        //   explorer context menus
+       // Optionally do things such as:
+       // - Add your .exe to the PATH
+       // - Write to the registry for things like file associations and
+       //   explorer context menus
         // Install desktop and start menu shortcuts
-        spawnUpdate(['--createShortcut', exeName]);
+       spawnUpdate(['--createShortcut', exeName]);
         setTimeout(app.quit, 1000);
-        return true;
+       return true;
       case '--squirrel-uninstall':
-        // Undo anything you did in the --squirrel-install and
-        // --squirrel-updated handlers
+       // Undo anything you did in the --squirrel-install and
+       // --squirrel-updated handlers
         // Remove desktop and start menu shortcuts
-        spawnUpdate(['--removeShortcut', exeName]);
+       spawnUpdate(['--removeShortcut', exeName]);
         setTimeout(app.quit, 1000);
-        return true;
+       return true;
       case '--squirrel-obsolete':
-        // This is called on the outgoing version of your app before
-        // we update to the new version - it's the opposite of
-        // --squirrel-updated
+       // This is called on the outgoing version of your app before
+       // we update to the new version - it's the opposite of
+       // --squirrel-updated
         app.quit();
-        return true;
-    }
+       return true;
+   }
 };
 
 app.on('ready', () => {
@@ -83,14 +79,7 @@ app.on('ready', () => {
     if (process.env.NODE_ENV !== 'production' && process.platform === 'win32') {
         ws.create("%APPDATA%/Microsoft/Windows/Start Menu/Programs/Electron.lnk", process.execPath);
         app.setAppUserModelId(process.execPath);
-    }
-    if (isDev === false) {
-        autoUpdater.setFeedURL(feed)
-        setInterval(() => {
-            autoUpdater.checkForUpdates();
-        }, 60000)
     } 
-        
 });
 
 ipcMain.on('timeUpdate', (event, timeUpdate) => {
@@ -101,21 +90,3 @@ ipcMain.on('timeUpdate', (event, timeUpdate) => {
     }
 });
 
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Reiniciar', 'Mais tarde'],
-      title: 'Atualização da Aplicação',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail: 'Uma nova versão foi recebida. Reinicie a aplicação para aplicar a atualização.'
-    }
-  
-    dialog.showMessageBox(dialogOpts, (response) => {
-      if (response === 0) autoUpdater.quitAndInstall()
-    })
-});
-
-autoUpdater.on('error', message => {
-    console.error('Houve um problema ao atualizar a aplicação')
-    console.error(message)
-});
